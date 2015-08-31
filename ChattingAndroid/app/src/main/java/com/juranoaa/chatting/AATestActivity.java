8bed441876edf7a99fcd4488d2d9d7b145a1c209
Francis <tjdrms0121@gmail.com>
2015-08-31 13:11:39 +0900

    init android web project

---
+++ ChattingAndroid/app/src/main/java/com/juranoaa/chatting/AATestActivity.java
@@ -0,0 +1,13 @@
+package com.juranoaa.chatting;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by SungGeun on 2015-08-31.
+ */
+@EActivity(R.layout.main)
+public class AATestActivity extends Activity {
+
+}
